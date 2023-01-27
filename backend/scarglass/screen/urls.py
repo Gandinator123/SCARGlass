baf184ceacfd8da240241c00884ec0ab7989a2f2
Chris Myers <cm1220@ic.ac.uk>
2023-01-27 23:00:02 +0000

    update view

--- backend/scarglass/screen/urls.py
+++ backend/scarglass/screen/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import ScreenList, ScreenCreate, ScreenDetail
+from .views import ScreenList, ScreenCreate, ScreenDetail, ScreenUpdate
 
 urlpatterns = [
     path('', ScreenList.as_view()),
     path('create/', ScreenCreate.as_view()),
     path('<int:pk>/', ScreenDetail.as_view()),
+    path('<int:pk>/update/', ScreenUpdate.as_view()),
 ]
--- backend/scarglass/screen/views.py
+++ backend/scarglass/screen/views.py
@@ -14,3 +14,10 @@ class ScreenCreate(generics.CreateAPIView):
 class ScreenDetail(generics.RetrieveAPIView):
   queryset = ScreenModel.objects.all()
   serializer_class = ScreenSerializer
+
+class ScreenUpdate(generics.UpdateAPIView):
+  queryset = ScreenModel.objects.all()
+  serializer_class = ScreenSerializer
+
+  def put(self, request, *args, **kwargs):
+    return self.partial_update(request, *args, **kwargs)
