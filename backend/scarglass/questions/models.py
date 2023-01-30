b35d745fcab12b60fe7443074d6a752117d08703
Chris Myers <cm1220@ic.ac.uk>
2023-01-30 19:13:08 +0000

    questions

--- backend/scarglass/questions/models.py
+++ backend/scarglass/questions/models.py
@@ -2,4 +2,4 @@ from django.db import models
 
 class QuestionModel(models.Model):
   question = models.CharField(max_length=255)
-  response = models.CharField(max_length=255)
+  response = models.TextField()
