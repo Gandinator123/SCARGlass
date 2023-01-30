16079ab3395f9c69acb1438fb4b7cd5996a0c051
sheriff4000 <sheriff4000@gmail.com>
2023-01-30 19:22:49 +0000

    .

--- backend/scarglass/photos/photo_test/img_to_txt2.py
+++ backend/scarglass/photos/photo_test/img_to_txt2.py
@@ -4,11 +4,10 @@ from googletrans import Translator
 
 translator = Translator()
 
-image = 'backend/scarglass/photos/photo_test/spanish.png'
+image = 'backend/scarglass/photos/photo_test/spanish.png' #img location
 text = pytesseract.image_to_string(Image.open(image), lang='eng+fra+spa')
 text = text.replace('\n', ' ').replace('\r', ' ')
 print("in spanish:", text)
-print(type(text))
 
 to_eng = translator.translate(text, dest ='en')
 out = to_eng.text.replace('\n', '').replace('\r', '')
