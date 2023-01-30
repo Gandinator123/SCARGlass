31cb7dc0ccaab509f46f2579f2fe9907e4aac54c
Chris Myers <cm1220@ic.ac.uk>
2023-01-30 10:58:45 +0000

    photos

--- backend/scarglass/photos/urls.py
+++ backend/scarglass/photos/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import PhotoList
+from .views import PhotoList, PhotoCreate
 
 urlpatterns = [
     path('', PhotoList.as_view()),
+    path('create/', PhotoCreate.as_view()),
 ]
