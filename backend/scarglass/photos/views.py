ea540aa19785ccfd3502ba02b08a4d399db23e7b
Chris Myers <cm1220@ic.ac.uk>
2023-02-01 13:53:10 +0000

    photo functions

--- backend/scarglass/photos/views.py
+++ backend/scarglass/photos/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from rest_framework import generics
+from rest_framework import generics, views, permissions, response, status
 from .models import PhotoModel
 from .serializer import PhotoSerializer
 
@@ -7,9 +7,14 @@ class PhotoList(generics.ListAPIView):
   queryset = PhotoModel.objects.all()
   serializer_class = PhotoSerializer
 
-class PhotoCreate(generics.CreateAPIView):
-  queryset = PhotoModel.objects.all()
-  serializer_class = PhotoSerializer
+class PhotoCreate(views.APIView):
+  def post(self, request, format=None):
+    request.data['text'] = "Some text!"
+    serializer = PhotoSerializer(data=request.data)
+    if serializer.is_valid():
+      serializer.save()
+      return response.Response(status=status.HTTP_200_OK)
+    return response.Response(status=status.HTTP_400_BAD_REQUEST)
 
 class PhotoDelete(generics.DestroyAPIView):
   queryset = PhotoModel.objects.all()
