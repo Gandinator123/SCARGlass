a4180c058079ace27e01279103fb42a133ae7994
Chris Myers <cm1220@ic.ac.uk>
2023-02-01 17:57:36 +0000

    ejghd

--- backend/scarglass/photos/views.py
+++ backend/scarglass/photos/views.py
@@ -11,7 +11,8 @@ class PhotoList(generics.ListAPIView):
 
 class PhotoCreate(views.APIView):
   def post(self, request, format=None):
-    text = translate(request.data['photo'])
+    # text = translate(request.data['photo'])
+    text = "eee"
     request.data._mutable = True
     request.data['text'] = text
     request.data._mutable = False
