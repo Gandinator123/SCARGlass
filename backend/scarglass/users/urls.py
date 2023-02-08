6de81eb6f6eff5327ff7acaf072178a59db41b7a
Chris Myers <cm1220@ic.ac.uk>
2023-02-08 17:35:46 +0000

    rhdfgj

--- backend/scarglass/users/urls.py
+++ backend/scarglass/users/urls.py
@@ -1,9 +1,14 @@
 from django.urls import path
 from .views import UserCreate, UserList, UserDetail, UserSignin
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
 
 urlpatterns = [
   path('', UserList.as_view(), name='User list'),
   path('register/', UserCreate.as_view(), name='User create'),
   path('signin/', UserSignin.as_view(), name='User sign in'),
   path('<int:pk>/', UserDetail.as_view(), name='User detail'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
