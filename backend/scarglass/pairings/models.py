81c230e4b18b325e7b7fba37324d2ac2a14f6816
Chris Myers <cm1220@ic.ac.uk>
2023-02-08 13:08:42 +0000

    pairings

--- backend/scarglass/pairings/models.py
+++ backend/scarglass/pairings/models.py
@@ -3,6 +3,7 @@ from django.db import models
 class PairingModel(models.Model):
   ip = models.CharField(max_length=255)
   paired = models.BooleanField(default=False)
+  date = models.DateTimeField(auto_now_add=True)
 
   def __str__(self):
       return self.ip
--- backend/scarglass/pairings/views.py
+++ backend/scarglass/pairings/views.py
@@ -1,10 +1,20 @@
-from rest_framework import generics
+from rest_framework import generics, response
 from .models import PairingModel
 from .serializers import PairingSerializer
+from django.db.models import Q
+from datetime import date, timedelta
 
 class PairingList(generics.ListAPIView):
-  queryset = PairingModel.objects.all()
-  serializer_class = PairingSerializer
+  def filter_queryset(self, queryset):
+    pair = self.request.query_params.get('pair')
+    five_minutes_ago = django.utils.timezone.now() + datetime.timedelta(minutes=-5)
+    if pair:
+      queryset = queryset.filter(Q(paired=True) & Q(date__gte = five_minutes_ago))
+
+  def list(self, request):
+    queryset = self.filter_queryset(self.get_queryset())
+    serializer = PairingSerializer(queryset, many=True)
+    return response.Response(serializer.data)
 
 class PairingCreate(generics.CreateAPIView):
   queryset = PairingModel.objects.all()
