ac6337afdc1df3b1b5562473c271f5e0a37b4631
Chris Myers <cm1220@ic.ac.uk>
2023-02-12 12:45:39 +0000

    dhifh

--- backend/scarglass/screen/serializers.py
+++ backend/scarglass/screen/serializers.py
@@ -1,8 +1,11 @@
 from rest_framework import serializers
 from .models import ScreenModel
+from users.serializers import UserSerializer
 
 class ScreenSerializer(serializers.ModelSerializer):
-  user = serializers.SlugRelatedField(slug_field='name')
+  # user = serializers.SlugRelatedField(slug_field='name')
+  user = UserSerializer()
+
   class Meta:
     model = ScreenModel
     fields = '__all__'
