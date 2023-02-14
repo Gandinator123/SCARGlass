ca261c2076b20888a7f6a95859ba9fda1fdcd182
Chris Myers <cm1220@ic.ac.uk>
2023-02-14 12:40:44 +0000

    idjb

--- backend/scarglass/photos/models.py
+++ backend/scarglass/photos/models.py
@@ -2,7 +2,8 @@ from django.db import models
 from screen.models import ScreenModel
 
 def upload_to(instance, filename):
-    return '{filename}'.format(filename=filename)
+  print(instance.screen)
+  return '{filename}'.format(filename=filename)
 
 class PhotoModel(models.Model):
   screen = models.ForeignKey(ScreenModel, on_delete=models.CASCADE, related_name="photos")
