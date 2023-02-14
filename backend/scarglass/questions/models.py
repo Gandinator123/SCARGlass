2c525184d4728cb997d844aeb74d9272e3c5b905
Chris Myers <cm1220@ic.ac.uk>
2023-02-14 12:22:43 +0000

    idjb

--- backend/scarglass/questions/models.py
+++ backend/scarglass/questions/models.py
@@ -1,5 +1,7 @@
 from django.db import models
+from screen.models import ScreenModel
 
 class QuestionModel(models.Model):
+  screen = models.ForeignKey(ScreenModel, on_delete=models.CASCADE, related_name="questions")
   question = models.CharField(max_length=255)
   response = models.TextField()
